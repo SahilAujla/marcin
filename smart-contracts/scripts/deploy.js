6570e2b4b98ef38acbd17a9a0485ed89e3756f69
Sahil Aujla <sahilaujla15@gmail.com>
2022-03-25 20:11:57 +0530

    Writing the smart contract.

---
+++ smart-contracts/scripts/deploy.js
@@ -0,0 +1,45 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
+
+async function main() {
+  /*
+ A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+ so randomWinnerGame here is a factory for instances of our RandomWinnerGame contract.
+ */
+
+  const metadataURL = "ipfs://QmPbcmBt12acAEGR4VzPqtQ55wB9EeLAao8zmqKFSSheKn/";
+
+  const marcinNFT = await ethers.getContractFactory("Marcin");
+  // deploy the contract
+  const deployedMarcinNFT = await marcinNFT.deploy(metadataURL);
+
+  await deployedMarcinNFT.deployed();
+
+  // print the address of the deployed contract
+  console.log("Verify Contract Address:", deployedMarcinNFT.address);
+
+  console.log("Sleeping.....");
+  // Wait for etherscan to notice that the contract has been deployed
+  await sleep(50000);
+
+  // Verify the contract after deploying
+  await hre.run("verify:verify", {
+    address: deployedMarcinNFT.address,
+    constructorArguments: [metadataURL],
+  });
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+// Deployed Contract Address:
